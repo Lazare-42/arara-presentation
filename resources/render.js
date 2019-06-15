67cce3c289b1e5292680d985f6639eed4d1eaf36
lazrossi <lazrossi@student.42.fr>
2019-06-15 16:15:34 -0400

    first working case with two distinct graphs rendering in parralel

--- resources/render.js
+++ resources/render.js
@@ -1,4 +1,10 @@
-function actionFromClick(nodes, dotSrc, index, svgNumber) {
+/**
+ *	Global variables : the name of the different graphs you can represent, which
+ *	are the same as the names of the files
+ */
+dotGraphs = ["main", "webView"]
+
+function actionFromClick(nodes, dotSrc, index, svgNumber, graphviz, svg) {
 
 	var dotSrcLines = dotSrc[index].split('\n');
 	var svg			= d3.select("#graph" + svgNumber)
@@ -8,41 +14,45 @@ function actionFromClick(nodes, dotSrc, index, svgNumber) {
 	var text 		= d3.select(nodes).selectAll('text').text();
 	var id 			= d3.select(nodes).attr('id');
 	var class1 		= d3.select(nodes).attr('class');
-
+	
 	dotElement = title.replace('->',' -> ');
-	console.log('Element id="%s" class="%s" title="%s" text="%s" dotElement="%s"', id, class1, title, text, dotElement);
-	console.log('Finding and deleting references to %s from the DOT source %s ', dotElement);
-	//for (i = 0; i < dotSrcLines.length;) {
-	//	if (dotSrcLines[i].indexOf(dotElement) >= 0) {
-	//		console.log('Deleting line %d: %s', i, dotSrcLines[i]);
-	//		dotSrcLines.splice(i, 1);
-	//	} else {
-	//		i++;
-	//	}
-	//}
-	//dotSrc = dotSrcLines.join('\n');
+	console.log('Element id="%s" class="%s" title="%s" text="%s" dotElement="%s\nSVG number="%d""', id, class1, title, text, dotElement, svgNumber);
+	console.log('Finding and deleting references to %s from the DOT source', dotElement);
 
-	if (index != dotSrc.length - 1) {
-	render(dotSrc, index + 1, svgNumber);
+
+	/**
+	 *	If the node title of the node on which we clicked on is a graph,
+	 *	start a new svg avtivity with that graph
+	 */
+	if (title == "androidWebView") {
+		getDot("webView", svgNumber + 1)
 	}
 	else {
-	render(dotSrc, 0, svgNumber);
+		render(dotSrc, index + 1, svgNumber, graphviz, svg)
 	}
+//	//for (i = 0; i < dotSrcLines.length;) {
+//	//	if (dotSrcLines[i].indexOf(dotElement) >= 0) {
+//	//		console.log('Deleting line %d: %s', i, dotSrcLines[i]);
+//	//		dotSrcLines.splice(i, 1);
+//	//	} else {
+//	//		i++;
+//	//	}
+//	//}
+//	//dotSrc = dotSrcLines.join('\n');
+//
 }
 
 function transition() {
-	d3.transition()
-		.ease(d3.easeLinear)
-		.delay(100)
-		.duration(1000);
+	var transition1	= d3.transition()
+						.ease(d3.easeLinear)
+						.delay(10)
+						.duration(1000);
+	return (transition1)
 }
 
-function render(dotSrc, index, svgNumber) {
+function render(dotSrc, index, svgNumber, graphviz, svg) {
 
 	console.log("This is the svg_to_select : " + "#graph" + svgNumber)
-
-	var svg			= d3.select("#graph" + svgNumber)
-	var graphviz 	= svg.graphviz()
 
 	graphviz
 		.engine("dot")
@@ -52,30 +62,29 @@ function render(dotSrc, index, svgNumber) {
 			nodes = svg.selectAll('.node,.edge');
 			nodes
 				.on("click", function () {
-					actionFromClick(this, dotSrc, index, svgNumber);
+					actionFromClick(this, dotSrc, index, svgNumber, graphviz, svg);
 				}
 				)}
 		)
 }
 
-function selectSvgForDot(digraphArray, svgNumber) {
+function selectSvgForDot(digraphArray, index, svgNumber) {
 
 	d3.select("#svg_container")
 		.append("graph")
 		.attr("id", "graph" + svgNumber)
-	render(digraphArray, 0, svgNumber)
+	var svg			= d3.select("#graph" + svgNumber)
+	var graphviz 	= svg.graphviz()
+	render(digraphArray, index, svgNumber, graphviz, svg)
 }
 
-function getDot() {
-	fetch("http://0.0.0.0:5000/resources/arara.dot").then(res => res.text()).then((allTheDot =>
+function getDot(graphName, svgNumber) {
+	console.log("Will try opening the file http://0.0.0.0:5000/resources/" + graphName + ".dot" + " with the svgNumber : " + svgNumber)
+	fetch("http://0.0.0.0:5000/resources/" + graphName + ".dot").then(res => res.text()).then((dotInfo =>
 		{
-			var digraphArray = allTheDot.split("/*CUT*/")
-			selectSvgForDot(digraphArray, 1)
-		}))
-	fetch("http://0.0.0.0:5000/resources/initial.dot").then(res => res.text()).then((allTheDot =>
-		{
-			var digraphArray = allTheDot.split("/*CUT*/")
-			selectSvgForDot(digraphArray, 2)
+			var digraphArray = dotInfo.split("/*CUT*/")
+			selectSvgForDot(digraphArray, 0, svgNumber)
 		}))
 }
-getDot()
+
+getDot("arara", 0)
