f42c4e633e471801036137751f7e4e6948e2b153
lazrossi <lazrossi@student.42.fr>
2019-06-15 11:13:58 -0400

    writing dot files

--- resources/render.js
+++ resources/render.js
@@ -1,23 +1,28 @@
 var dotIndex = 0;
-var graphviz = d3.select("#svg_container").graphviz()
+var svg			= d3.select("#svg_container")
+var graphviz 	= svg.graphviz()
 
 function actionFromClick(nodes, dotSrc, index) {
-//	var title = d3.select(nodes).selectAll('title').text().trim();
-//	var text = d3.select(nodes).selectAll('text').text();
-//	var id = d3.select(nodes).attr('id');
-//	var class1 = d3.select(nodes).attr('class');
-//	dotElement = title.replace('->',' -> ');
-//	console.log('Element id="%s" class="%s" title="%s" text="%s" dotElement="%s"', id, class1, title, text, dotElement);
-//	console.log('Finding and deleting references to %s "%s" from the DOT source', class1, dotElement);
-//	for (i = 0; i < dotSrcLines.length;) {
-//		if (dotSrcLines[i].indexOf(dotElement) >= 0) {
-//			console.log('Deleting line %d: %s', i, dotSrcLines[i]);
-//			dotSrcLines.splice(i, 1);
-//		} else {
-//			i++;
-//		}
-//	}
-//	dotSrc = dotSrcLines.join('\n');
+
+	var dotSrcLines = dotSrc[index].split('\n');
+	var title 		= d3.select(nodes).selectAll('title').text().trim();
+	var text 		= d3.select(nodes).selectAll('text').text();
+	var id 			= d3.select(nodes).attr('id');
+	var class1 		= d3.select(nodes).attr('class');
+
+	dotElement = title.replace('->',' -> ');
+	console.log('Element id="%s" class="%s" title="%s" text="%s" dotElement="%s"', id, class1, title, text, dotElement);
+	console.log('Finding and deleting references to %s from the DOT source %s ', dotElement);
+	//for (i = 0; i < dotSrcLines.length;) {
+	//	if (dotSrcLines[i].indexOf(dotElement) >= 0) {
+	//		console.log('Deleting line %d: %s', i, dotSrcLines[i]);
+	//		dotSrcLines.splice(i, 1);
+	//	} else {
+	//		i++;
+	//	}
+	//}
+	//dotSrc = dotSrcLines.join('\n');
+
 	if (index != dotSrc.length - 1) {
 	render(dotSrc, index + 1);
 	}
@@ -27,9 +32,6 @@ function actionFromClick(nodes, dotSrc, index) {
 }
 
 function render(dotSrc, index) {
-	console.log('DOT source =', dotSrc[index]);
-	var dotSrcLines = dotSrc[index].split('\n');
-	console.log(dotSrcLines)
 
 	transition1 = d3.transition()
 		.delay(100)
