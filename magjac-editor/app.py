dc01eaf26caee89c9427c0e3802557fb8542b083
lazrossi <lazrossi@student.42.fr>
2019-06-20 04:43:03 -0400

    Unified background color

---
+++ magjac-editor/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, send_from_directory, request, make_response, jsonify
+from flask_cors import CORS
+import sys
+
+app = Flask(__name__, template_folder='templates', static_url_path='')
+
+CORS(app)
+
+@app.route("/", methods=['GET'])
+def index():
+    print(request.headers, file=sys.stderr)
+    response        = make_response(render_template('index.html'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/resources/<path:filename>', methods=['GET'])
+def send_file(filename):
+    print(request.headers, file=sys.stderr)
+    answer = send_from_directory('resources', filename)
+    answer.headers.set('Access-Control-Allow-Origin', '*')
+    return answer
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=50)
--- resources/render.js
+++ resources/render.js
@@ -11,215 +11,198 @@ var previouslyClickedElementId = null
 var previousSvg                = null
 
 function escapeXml(unsafe) {
-    return unsafe.replace(/[<>&'"]/g, function (c) {
-        switch (c) {
-            case '<': return '&lt;';
-            case '>': return '&gt;';
-            case '&': return '&amp;';
-            case '\'': return '&apos;';
-            case '"': return '&quot;';
-        }
-    });
+	return unsafe.replace(/[<>&'"]/g, function (c) {
+		switch (c) {
+			case '<': return '&lt;';
+			case '>': return '&gt;';
+			case '&': return '&amp;';
+			case '\'': return '&apos;';
+			case '"': return '&quot;';
+		}
+	});
 }
 
+/**
+ *	Load code corresponding to the clicked button
+ */
+function loadCode(clickedLink) {
 
-    /**
-     *	Load code corresponding to the clicked button
-     */
-    function loadCode(clickedLink) {
+	var Element 		= document.getElementById('presentationCode');
 
-        var Element 		= document.getElementById('presentationCode');
+	//console.log("clicked : " + clickedLink)
+	$("#presentationCode").hide()
+	if (( index = codeFiles.findIndex(x => x.includes(clickedLink))) != -1) {
+		// randomize the query to never get the cached version of the file
+		//fetch("/resources/code/" + "arrayExamples.java" + "?" + performance.now())
+		fetch("/resources/code/" + codeFiles[index] + "?" + performance.now())
+			.then(res => res.text())
+			.then(res => escapeXml(res))
+			.then(res => "<pre><code>\n" + res + "</pre></code>")
+			.then(res =>
+				{
+					Element.innerHTML 	= res
+					return res
+				})
+				.then(res => {
+					hljs.initHighlighting.called = false;
+					hljs.initHighlighting();
+					$("#presentationCode").fadeIn(1500)
+				})
+	}
+}
 
-        //console.log("clicked : " + clickedLink)
-        $("#presentationCode").hide()
-        if (( index = codeFiles.findIndex(x => x.includes(clickedLink))) != -1) {
-            // randomize the query to never get the cached version of the file
-            //fetch("/resources/code/" + "arrayExamples.java" + "?" + performance.now())
-            fetch("/resources/code/" + codeFiles[index] + "?" + performance.now())
-                .then(res => res.text())
-			    .then(res => escapeXml(res))
-                .then(res => "<pre><code>\n" + res + "</pre></code>")
-                .then(res =>
-                    {
-                        Element.innerHTML 	= res
-                        return res
-                    })
-                .then(res => {
-                    hljs.initHighlighting.called = false;
-                    hljs.initHighlighting();
-                    $("#presentationCode").fadeIn(1500)
-                })
-        }
-    }
+/**
+ * Actions on Click
+ */
+function actionFromClick(clickedNode, dotSrc, index, svgNumber, graphviz, svg, currentGraphName) {
 
-    /**
-     * Actions on Click
-     */
-    function actionFromClick(clickedNode, dotSrc, index, svgNumber, graphviz, svg, currentGraphName) {
+	var dotSrcLines = dotSrc[index].split('\n');
 
-        var dotSrcLines = dotSrc[index].split('\n');
+	var title 		= d3.select(clickedNode).selectAll('title').text().trim();
+	var text 		= d3.select(clickedNode).selectAll('text').text();
+	var id 			= d3.select(clickedNode).attr('id');
+	var class1 		= d3.select(clickedNode).attr('class');
+	var shape 		= d3.select(clickedNode).attr('shape');
 
-        var title 		= d3.select(clickedNode).selectAll('title').text().trim();
-        var text 		= d3.select(clickedNode).selectAll('text').text();
-        var id 			= d3.select(clickedNode).attr('id');
-        var class1 		= d3.select(clickedNode).attr('class');
-        var shape 		= d3.select(clickedNode).attr('shape');
+	/**
+	 *  If a node or link was previously selected ; reset it to default
+	 *  Only apply if not a menu button
+	 */
+	if(title != "Previous" && title != "Next" && title != "Delete") {
+		if (previouslyClickedElementId != null && previousSvg == svg) {
+			svg.select('#' + previouslyClickedElementId + ' :not(title):not(text)')
+				.transition()
+				.duration(1500)
+				.attr("fill", "#ffc3a0")
+		}
 
-        /**
-         *  If a node or link was previously selected ; reset it to default
-         *  Only apply if not a menu button
-         */
-        if(title != "Previous" && title != "Next" && title != "Delete") {
-            if (previouslyClickedElementId != null && previousSvg == svg) {
-                svg.select('#' + previouslyClickedElementId + ' :not(title):not(text)')
-                    .transition()
-                    .duration(1500)
-                    .attr("fill", "grey")
-            }
+		console.log("Transition for " + '#' + id + ' :not(title):not(text)')
+		svg.select('#' + id + ' :not(title):not(text)')
+			.transition()
+			.duration(1500)
+			.attr("fill", "#ffb347")
 
-            console.log("Transition for " + '#' + id + ' :not(title):not(text)')
-            svg.select('#' + id + ' :not(title):not(text)')
-                .transition()
-                .duration(1500)
-                .attr("fill", "#ffb347")
+		previouslyClickedElementId = id
+		previousSvg                = svg
+	}
 
-            previouslyClickedElementId = id
-            previousSvg                = svg
-        }
+	/**
+	 *	You could open new graphs on click. Removed this possibility for this presentation
+	 */
+	if (dotGraphs.indexOf(text) > 0 && currentGraphName != text) {
+//		/**
+//		 *	Remove existing graphs after the current graph if they exist
+//		 */
+//		var nextSvg = svgNumber + 1
+//		if (d3.select("#graph" + nextSvg).empty() == true) {
+//			return getDot(title, svgNumber + 1)
+//		}
+//		else {
+//			while (d3.select("#graph" + nextSvg).empty() == false) {
+//				d3.select("#graph" + nextSvg).remove()
+//				nextSvg += 1
+//			}
+//			return getDot(title, svgNumber + 1)
+//		}
+	}
+	else {
+		loadCode(title)
+		if (index != 0 && title == "Previous") {
+			addDigraphTitle(dotSrc, index - 1, svgNumber, graphviz, svg, currentGraphName)
+		}
+		if (index != dotSrc.length - 1 && title == "Next") {
+			addDigraphTitle(dotSrc, index + 1, svgNumber, graphviz, svg, currentGraphName)
+		}
+		if (title == "Delete") {
+			svg.remove()
+		}
+	}
+}
 
-        if (dotGraphs.indexOf(text) > 0 && currentGraphName != text) {
-            /**
-             *	Remove existing graphs after the current graph if they exist
-             */
-            var nextSvg = svgNumber + 1
-            if (d3.select("#graph" + nextSvg).empty() == true) {
-                return getDot(title, svgNumber + 1)
-            }
-            else {
-                while (d3.select("#graph" + nextSvg).empty() == false) {
-                    d3.select("#graph" + nextSvg).remove()
-                    nextSvg += 1
-                }
-                return getDot(title, svgNumber + 1)
-            }
-        }
-        else {
-            loadCode(title)
-            if (index != 0 && title == "Previous") {
-                addDigraphTitle(dotSrc, index - 1, svgNumber, graphviz, svg, currentGraphName)
-            }
-            if (index != dotSrc.length - 1 && title == "Next") {
-                addDigraphTitle(dotSrc, index + 1, svgNumber, graphviz, svg, currentGraphName)
-            }
-            if (title == "Delete") {
-                svg.remove()
-            }
-        }
-    }
+/**
+ *	Transition function between two graph states
+ */
+function transition() {
+	var transition1	= d3.transition()
+		.ease(d3.easeLinear)
+		.delay(10)
+		.duration(1000);
+	return (transition1)
+}
 
-    /**
-     *	Transition function between two graph states
-     */
-    function transition() {
-        var transition1	= d3.transition()
-                            .ease(d3.easeLinear)
-                            .delay(10)
-                            .duration(1000);
-        return (transition1)
-    }
+function attributer(datum, index, nodes, svg) {
+	var selection = d3.select(this);
+	if (datum.tag == "svg") {
+		var width = 	svg.node().clientWidth;
+		var height = 	svg.node().clientHeight;
+		var x 		= "10";
+		var y 		= "10";
+		var unit 	= 'px';
+		selection
+			.attr("width", width + unit)
+			.attr("height", height + unit)
+		datum.attributes.width = width + unit;
+		datum.attributes.height = height + unit;
+	}
+	if (datum.tag == "ellipse") {
+		selection.attributes.fill = "red"
+	}
+	console.log(datum.tag)
+	//nodes.selectAll(nodes).attr("color", "blue")
+}
 
-    function attributer(datum, index, nodes, svg) {
-        var selection = d3.select(this);
-        if (datum.tag == "svg") {
-            var width = 	svg.node().clientWidth;
-            var height = 	svg.node().clientHeight;
-            var x 		= "10";
-            var y 		= "10";
-            var unit 	= 'px';
-            selection
-                .attr("width", width + unit)
-                .attr("height", height + unit);
-            datum.attributes.width = width + unit;
-            datum.attributes.height = height + unit;
-        }
-        d3.select('#' + "graph0" + ' polygon')
-            .attr("fill", "blue")
-    }
+/**
+ *	Dot rendering with D3 
+ *	You can choose the graphviz engine to render the Dot
+ */
+function render(dotSrc, index, svgNumber, graphviz, svg, currentGraphName) {
+	return new Promise((resolve, reject) => {
+		var dotTorender = dotSrc[index]
 
-    function attributer2(datum) {
-//        d3.select('#' + "graph0" + ' polygon')
-//            .attr("fill", "blue")
-    }
+		graphviz
+			.engine("dot")
+			.attributer(attributer(svg))
+			.transition(transition())
+			.renderDot(dotTorender)
 
-    /**
-     *	Dot rendering with D3 
-     *	You can choose the graphviz engine to render the Dot
-     */
-    function render(dotSrc, index, svgNumber, graphviz, svg, currentGraphName) {
-        return new Promise((resolve, reject) => {
-            var dotTorender = dotSrc[index]
+			.on("end", d    =>   {
 
-            graphviz
-                .engine("dot")
-                .attributer(attributer2(svg))
-                .transition(transition())
-                .attributer(attributer(svg))
-                .renderDot(dotTorender)
+				document.getElementById("graph" + svgNumber).scrollIntoView({ block: 'end',  behavior: 'smooth' })
+				nodes = svg.selectAll('.node,.edge');
+				nodes
+					.on("click", function () {
+						actionFromClick(this, dotSrc, index, svgNumber, graphviz, svg, currentGraphName)
+					})
+				resolve()
+			}
+			)
+	})
+}
 
-                .on("end", d    =>   {
+function selectSvgForDot(digraphArray, index, svgNumber, currentGraphName) {
 
-                    //  ERICK HELP
-                    document.getElementById("graph" + svgNumber).scrollIntoView({ block: 'end',  behavior: 'smooth' })
+	d3.select("#svg_container")
+		.append("graph")
+		.attr("id", "graph" + svgNumber)
 
+	var svg			= d3.select("#graph" + svgNumber)
+	var graphviz 	= svg.graphviz()
 
-                    //parentSvg = document.getElementById("graph" + svgNumber)
-                    //var children = parentSvg.children;
+	return addDigraphTitle(digraphArray, index, svgNumber, graphviz, svg, currentGraphName)
+}
 
-                    //for (i = 0; i < children.length; i++) {
-                    //    console.log("Looping through " + children[i])
-                    //}
+function getDot(graphName, svgNumber) {
+	return fetch("/resources/dot/" + graphName + ".dot" + "?" + performance.now())
+		.then(res => res.text())
+		.then(res => res.replace(/\t+/g, ' ').trim())
+		.then((dotInfo =>
+			{
+				var currentGraphName = graphName
+				var digraphArray = dotInfo.split("/*CUT*/")
+				//console.log("Created  : " + digraphArray[0])
+				return selectSvgForDot(digraphArray, 0, svgNumber, currentGraphName)
+			}))
+			.then(loadCode.bind(null, graphName))
+}
 
-                    document.getElementById("graph" + svgNumber).style.backgroundColor="blue"
-                    svg.select('#' + "graph0" + ' polygon')
-                        .attr("fill", "blue")
-                            
-                    nodes = svg.selectAll('.node,.edge');
-                    nodes
-                        .on("click", function () {
-                            actionFromClick(this, dotSrc, index, svgNumber, graphviz, svg, currentGraphName)
-                        })
-                    resolve()
-                }
-                )
-        })
-    }
-
-    function selectSvgForDot(digraphArray, index, svgNumber, currentGraphName) {
-
-        d3.select("#svg_container")
-            .append("graph")
-            .attr("id", "graph" + svgNumber)
-
-        var svg			= d3.select("#graph" + svgNumber)
-        var graphviz 	= svg.graphviz()
-
-        return addDigraphTitle(digraphArray, index, svgNumber, graphviz, svg, currentGraphName)
-    }
-
-    function getDot(graphName, svgNumber) {
-        // randomize the query to never get the cached version of the file
-        //console.log("fetching..." + "/resources/dot/" + graphName + ".dot")
-        return fetch("/resources/dot/" + graphName + ".dot" + "?" + performance.now())
-            .then(res => res.text())
-            .then(res => res.replace(/\t+/g, ' ').trim())
-            .then((dotInfo =>
-                {
-                    var currentGraphName = graphName
-                    var digraphArray = dotInfo.split("/*CUT*/")
-                    //console.log("Created  : " + digraphArray[0])
-                    return selectSvgForDot(digraphArray, 0, svgNumber, currentGraphName)
-                }))
-                .then(loadCode.bind(null, graphName))
-    }
-
-    getDot(dotGraphs[0], 0)
+getDot(dotGraphs[0], 0)
